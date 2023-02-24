d13aa70228ae2dcb164003ea3e9ee8b837c41db5
Bijan Zarean <Bijanzarean@gmail.com>
2023-02-23 21:43:08 -0500

    I finished my work lolol

--- test.java
+++ test.java
@@ -1,7 +1,8 @@
 public class test{
 
 	public static void man(String[] args){
-		System.out.println("Changes from SDET-2");
+		System.out.println("Changes from dev2");
+		System.out.println("More changes from dev2");
 
 	}
 
